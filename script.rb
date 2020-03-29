42b8395f0ca5797870173e39cd7e24d4ad1a43af
ts-3156 <ts_3156@yahoo.co.jp>
2020-03-29 23:50:44 +0900

    Update script.rb

--- script.rb
+++ script.rb
@@ -3,35 +3,40 @@ require 'nokogiri'
 require 'chunky_png'
 require 'twitter'
 
-colors = []
+def generate_colors
+  if File.exist?('colors.json')
+    return JSON.parse(File.read('colors.json'))
+  end
 
-html = Nokogiri::HTML(URI.open(urls[0]).read)
-html.xpath('html/body/div[2]/table/tr/td/a/@title').each do |tag|
-  kanji, yomi, code = tag.text.strip.split(/\s/)
-  colors << {kanji: kanji, yomi: yomi, code: code}
-end
-
-
-html = Nokogiri::HTML(URI.open(urls[1]).read)
-html.xpath('//*[@id="mw-content-text"]/div/table').each.with_index do |table, table_i|
-  next if table_i == 0
-
-  table.xpath('tbody/tr').each.with_index do |tr, tr_i|
-    next if tr_i == 0
-
-    cells = tr.xpath('td')
-    kanji, yomi, code = cells[0].text.strip, nil, cells[4].text.strip.downcase
+  colors = []
+  
+  html = Nokogiri::HTML(URI.open(urls[0]).read)
+  html.xpath('html/body/div[2]/table/tr/td/a/@title').each do |tag|
+    kanji, yomi, code = tag.text.strip.split(/\s/)
     colors << {kanji: kanji, yomi: yomi, code: code}
-
-    if cells[5]
-      kanji, yomi, code = cells[5].text.strip, nil, cells[9].text.strip.downcase
+  end
+  
+  html = Nokogiri::HTML(URI.open(urls[1]).read)
+  html.xpath('//*[@id="mw-content-text"]/div/table').each.with_index do |table, table_i|
+    next if table_i == 0
+  
+    table.xpath('tbody/tr').each.with_index do |tr, tr_i|
+      next if tr_i == 0
+  
+      cells = tr.xpath('td')
+      kanji, yomi, code = cells[0].text.strip, nil, cells[4].text.strip.downcase
       colors << {kanji: kanji, yomi: yomi, code: code}
+  
+      if cells[5]
+        kanji, yomi, code = cells[5].text.strip, nil, cells[9].text.strip.downcase
+        colors << {kanji: kanji, yomi: yomi, code: code}
+      end
     end
   end
+  
+  File.write('colors.json', JSON.pretty_generate(colors.uniq { |c| c[:kanji] }))
+  JSON.parse(File.read('colors.json'))
 end
-
-File.write('colors.json', JSON.pretty_generate(colors.uniq { |c| c[:kanji] }))
-colors = JSON.parse(File.read('colors.json'))
 
 def generate_image(code, width: 1200, height: 600)
   png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
@@ -46,14 +51,26 @@ def generate_image(code, width: 1200, height: 600)
   png
 end
 
-colors.each.with_index do |color, i|
-  png = generate_image(color['code'])
+def tweet(client, kanji, code)
+  png = generate_image(code)
   filename = 'filename.png'
   png.save(filename, interlace: true)
-  client.update_with_media(color['kanji'], File.open(filename))
+  client.update_with_media("#{kanji} #{code}", File.open(filename))
+ensure
   File.delete(filename)
-  puts "#{i} #{color['kanji']}"
+end
 
-  sleep 100
+def main
+  colors = generate_colors
+  colors.uniq{ |c| c['kanji'] }.shuffle.each.with_index do |color, i|
+    tweet(client, color['kanji'], color['code'])
+    puts "#{i} #{Time.now} #{color['kanji']}"
+
+    sleep 3600
+  end
+end
+
+if __FILE__ == $0
+  main
 end
 
